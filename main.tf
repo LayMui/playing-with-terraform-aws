82e1b87a5f524232f302680e6d7c78c9723bfd59
Lay Mui <mui_ume@hotmail.com>
2020-07-17 17:01:47 +0800

    Add files via upload

--- main.tf
+++ main.tf
@@ -8,12 +8,25 @@ variable "im_user_name_prefix" {
     default = "my_iam_users"
 }
 
-variable "names" {
-    default = [ "john", "mary", "james" ]
+variable "users" {
+  //  default = [ "jane", "john", "mary", "james" ]
+  default = {
+    jane: { country: "singapore", department: "Business Development"},
+    john: { country: "malaysia", department: "Marketing" },
+    mary: { country: "new zealand", department: "Engineering"},
+    james: { country: "china", department: "IT"}
+  }
 }
 
 resource "aws_iam_user" "my_iam_users" {
-  count = length(var.names)
+  #count = length(var.names) # using count the update/delete will be based on index
   #name  = "${var.im_user_name_prefix}_${count.index}"
-  name = var.names[count.index]
+  #name = var.names[count.index]
+#  for_each = toset(var.names) #using for_each, the update/delete will be based on value in the set, index_key is the value
+  for_each = var.users
+    name = each.key
+    tags = {
+      country: each.value.country
+      department: each.value.department
+    }
 }
